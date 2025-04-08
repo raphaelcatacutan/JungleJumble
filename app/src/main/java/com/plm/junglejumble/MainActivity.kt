5db5932b1fcd3eaf7d7c91b386437d0550758bda
elbrauq <90502373+elbrauq@users.noreply.github.com>
2025-04-08 20:01:37 +0800

    Test Hello World!

--- app/src/main/java/com/plm/junglejumble/MainActivity.kt
+++ app/src/main/java/com/plm/junglejumble/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             JungleJumbleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "World!",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
