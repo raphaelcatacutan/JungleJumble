05d83272e3fdf489b2b17d4d80bf6ddb1f34afd1
raphaelcatacutan <raphaelcaspe.catacutan@gmail.com>
2025-04-09 15:48:25 +0800

    Test Git Username

--- app/src/main/java/com/plm/junglejumble/MainActivity.kt
+++ app/src/main/java/com/plm/junglejumble/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             JungleJumbleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "World!",
+                        name = "Test!",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
